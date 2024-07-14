077ad96181075a77f56c4bda5584d103b5112469
darshi1337 <priyadarshi.annupam.mec22@itbhu.ac.in>
2024-07-14 18:37:19 +0530

    fixed

--- src/text_extract.py
+++ src/text_extract.py
@@ -1,21 +1,34 @@
 from ebooklib import epub
+from bs4 import BeautifulSoup
+import os
 
-def extract_text(epub_file):
+def extract(epub_file):
     book = epub.read_epub(epub_file)
     text = []
     for item in book.get_items():
         if isinstance(item, epub.EpubHtml):
-            content = item.get_content().decode('utf-8')
-            text.append(content)
+            content = item.get_content()
+            decoded_content = content.decode('utf-8', errors='ignore')
+            soup = BeautifulSoup(decoded_content, 'html.parser')
+            cleaned_text = soup.get_text(separator='\n', strip=True)
+            text.append(cleaned_text)
     return '\n'.join(text)
 
+def new_file(text, file_path):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(text)
+
 epubs = [
-    r'C:\Users\DARSHI\Desktop\Step_Task\data\epub_files\Stephen-King-The-Stand_-The-Complete-_-Uncut-Edition-Doubleday-_1990_.epub',
-    r'C:\Users\DARSHI\Desktop\Step_Task\data\epub_files\Stephen King - Misery-Signet (1998).epub',
-    r'C:\Users\DARSHI\Desktop\Step_Task\data\epub_files\Stephen King - It_ A Novel-Signet (2009).epub'
+    r'C:\Users\DARSHI\Desktop\Step_Task\epub_files\Stephen-King-The-Stand_-The-Complete-_-Uncut-Edition-Doubleday-_1990_.epub',
+    r'C:\Users\DARSHI\Desktop\Step_Task\epub_files\Stephen King - Misery-Signet (1998).epub',
+    r'C:\Users\DARSHI\Desktop\Step_Task\epub_files\Stephen King - It_ A Novel-Signet (2009).epub'
 ]
 
-for epub_file in epubs:
-    extracted_text = extract_text(epub_file)
-    print(f"Text extracted from '{epub_file}':")
-    print(extracted_text)
+output_dir = 'extracted_text'
+os.makedirs(output_dir, exist_ok=True)
+
+for i, epub_file in enumerate(epubs):
+    extracted_text = extract(epub_file)
+    text_file_path = os.path.join(output_dir, f'ebook_{i+1}.txt')
+    new_file(extracted_text, text_file_path)
+    print(f"Text extracted from '{epub_file}' and saved to '{text_file_path}'")
